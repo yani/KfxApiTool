88ea2fc7dc424f6290c4f59a02f573334c2ee74f
Yani <yani@protonmail.com>
2024-06-25 03:31:42 +0200

    implement easier versioning + set version to 1.0.1

--- about_dialog.cpp
+++ about_dialog.cpp
@@ -1,11 +1,14 @@
 #include "about_dialog.h"
 #include "ui_about_dialog.h"
+#include "version.h"
 
 AboutDialog::AboutDialog(QWidget *parent)
     : QDialog(parent)
     , ui(new Ui::AboutDialog)
 {
     ui->setupUi(this);
+
+    ui->label_2->setText("KeeperFX API Tool v" + QString(APP_VERSION));
 }
 
 AboutDialog::~AboutDialog()
